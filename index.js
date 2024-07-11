f2f885d34184342380c384d91d1564cfa2f913db
Emma <emmaminary@gmail.com>
2024-07-11 16:59:18 +0200

    à reprendre gradient background

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
   var granimInstance = new Granim({
-    element: "#logo-canvas",
-    direction: "left-right",
+    element: "#granimCanvas",
+    direction: "diagonal",
     states: {
       "default-state": {
         gradients: [
-          ["#EB3349", "#F45C43"],
-          ["#FF8008", "#FFC837"],
-          ["#4CB8C4", "#3CD3AD"],
-          ["#24C6DC", "#514A9D"],
-          ["#FF512F", "#DD2476"],
-          ["#DA22FF", "#9733EE"],
+          ["#EB3349", "#F45C43"], // Rouge à orange
+          ["#FF8008", "#FFC837"], // Orange à jaune
+          ["#4CB8C4", "#3CD3AD"], // Cyan à vert
         ],
-        transitionSpeed: 2000,
+        transitionSpeed: 3000, // Transition plus lente
       },
     },
   });
